7fa48b7b10c06c9e39c2b265a33741e73a25b3d6
Fewsrt <chonnaphat.v@gmail.com>
2026-04-17 17:32:41 +0700

    Remove Firebase web config from source

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,8 +1,1 @@
-window.FIREBASE_CONFIG = {
-  projectId: "cert-expire-2026-ca",
-  appId: "1:195648781548:web:7f47e35506622365ff79c9",
-  storageBucket: "cert-expire-2026-ca.firebasestorage.app",
-  apiKey: "REMOVED_FIREBASE_API_KEY",
-  authDomain: "cert-expire-2026-ca.firebaseapp.com",
-  messagingSenderId: "195648781548"
-};
+window.FIREBASE_CONFIG = null;
